e18963741a20be8fbb35ad694f4335b4af17bbf5
danny <danny@dylar.betelgeuse.x>
2023-10-20 09:51:03 +1100

    removed obj files

--- src/obj/ssd1306.d
+++
@@ -1,8 +0,0 @@
-obj/ssd1306.o: ../SSD1306/lib/ssd1306.c ../SSD1306/lib/ssd1306.h \
- ../SSD1306/lib/font.h ../SSD1306/lib/twi.h
-
-../SSD1306/lib/ssd1306.h:
-
-../SSD1306/lib/font.h:
-
-../SSD1306/lib/twi.h:
--- src/obj/twi.d
+++
@@ -1,3 +0,0 @@
-obj/twi.o: ../SSD1306/lib/twi.c ../SSD1306/lib/twi.h
-
-../SSD1306/lib/twi.h:
